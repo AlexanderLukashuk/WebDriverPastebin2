3564481f257aff2add18f4f43e7717ae783fa525
AlexanderLukashuk <upiter465@gmail.com>
2023-12-04 12:47:34 +0600

    Initial Commit

---
+++ WebDriverpaste2.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace WebDriverpaste2.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
